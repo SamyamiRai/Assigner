454c32c2a72398f27fed238f888f428aa93315eb
anvitha340 <anvithashetty2000@gmail.com>
2021-07-24 17:14:14 +0530

    Connected register and login to database

--- app/src/main/java/com/example/assignerapp/LoginActivity.java
+++ app/src/main/java/com/example/assignerapp/LoginActivity.java
@@ -1,12 +1,23 @@
 package com.example.assignerapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -54,6 +65,4 @@ public class LoginActivity extends AppCompatActivity {
         tabLayout.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(100).start();
 
     }
-
-
 }
--- app/src/main/java/com/example/assignerapp/SignupTabFragment.java
+++ app/src/main/java/com/example/assignerapp/SignupTabFragment.java
@@ -1,25 +1,77 @@
 package com.example.assignerapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import org.jetbrains.annotations.NotNull;
 
-public class SignupTabFragment extends Fragment {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
+public class SignupTabFragment extends Fragment {
+    private EditText email_reg;
+    private EditText password_reg;
+    private Button register;
+
+    private FirebaseAuth auth;
 
     @Override
     public View onCreateView(@NonNull @NotNull LayoutInflater inflater, @Nullable @org.jetbrains.annotations.Nullable ViewGroup container, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
-        ViewGroup root= (ViewGroup) inflater.inflate(R.layout.signup_tab_fragment, container, false);
+        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.signup_tab_fragment, container, false);
 
-        return  root;
+        email_reg = root.findViewById(R.id.email1);
+        password_reg = root.findViewById(R.id.password1);
+        register = root.findViewById((R.id.register1));
+
+        auth = FirebaseAuth.getInstance();
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String txt_email = email_reg.getText().toString();
+                String txt_password = password_reg.getText().toString();
+
+                if (TextUtils.isEmpty(txt_email) || TextUtils.isEmpty(txt_password)) {
+                    Toast.makeText(getActivity(), "Empty Credentials!", Toast.LENGTH_SHORT).show();
+                } else if (txt_password.length() < 6) {
+                    Toast.makeText(getActivity(), "Password too Short!", Toast.LENGTH_SHORT).show();
+                } else {
+                    registerUser(txt_email, txt_password);
+                }
+            }
+        });
+        return root;
+    }
+
+    private void registerUser(String txt_email, String txt_password) {
+        auth.createUserWithEmailAndPassword(txt_email, txt_password).addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(getActivity(), "Register Successful!", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(getActivity(), MainActivity.class));
+                    getActivity().finish();
+                } else {
+                    Toast.makeText(getActivity(), "Failed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
+
+
+
