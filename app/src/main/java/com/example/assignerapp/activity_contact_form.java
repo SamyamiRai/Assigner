73979d0f44ab301b3677a411137fc351f1f2e0ba
anvitha340 <anvithashetty2000@gmail.com>
2021-07-27 12:16:07 +0530

    Image Upload Backend

--- app/src/main/java/com/example/assignerapp/activity_contact_form.java
+++ app/src/main/java/com/example/assignerapp/activity_contact_form.java
@@ -1,19 +1,34 @@
 package com.example.assignerapp;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActionBar;
+import android.app.ProgressDialog;
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.MimeTypeMap;
+import android.widget.AbsListView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import org.jetbrains.annotations.NotNull;
 
@@ -24,7 +39,9 @@ public class activity_contact_form extends AppCompatActivity {
     private EditText id, name, email,dept;
     private Button submit;
     String id_facuty;
-    private Button logout;
+    private Button logout,add_image;
+    private Uri imageUri;
+    private static final int IMAGE_REQUEST = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +53,8 @@ public class activity_contact_form extends AppCompatActivity {
         email = findViewById(R.id.faculty_email);
         dept = findViewById(R.id.faculty_dept);
         submit = findViewById(R.id.submit);
+        add_image = findViewById(R.id.add_image);
+
         FirebaseFirestore db = FirebaseFirestore.getInstance();
 
         submit.setOnClickListener(new View.OnClickListener() {
@@ -69,5 +88,77 @@ public class activity_contact_form extends AppCompatActivity {
                 startActivity(new Intent(activity_contact_form.this, LoginActivity.class));
             }
         });
+
+        add_image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openImage();
+            }
+        });
+    }
+
+    private void openImage() {
+        Intent intent = new Intent();
+        intent.setType("image/");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, IMAGE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable @org.jetbrains.annotations.Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == IMAGE_REQUEST && resultCode == RESULT_OK){
+            imageUri = data.getData();
+
+            uploadImage();
+        }
+    }
+
+    private  String getFileExtension (Uri uri) {
+        ContentResolver contentResolver = getContentResolver();
+
+        MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
+        return  mimeTypeMap.getExtensionFromMimeType(contentResolver.getType(uri));
+    }
+
+    private void uploadImage() {
+
+        if(imageUri != null) {
+            if (id.getText().toString().equals("")) {
+                Toast.makeText(this, "Enter Faculty ID ", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(activity_contact_form.this, activity_contact_form.class));
+            } else {
+                ProgressDialog pd = new ProgressDialog(this);
+                pd.setMessage("Uploading");
+                pd.show();
+                final StorageReference fileRef = FirebaseStorage.getInstance().getReference().child("uploads").child(id.getText().toString() + "." + getFileExtension(imageUri));
+
+                fileRef.putFile(imageUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                    LinearLayout l1 =new LinearLayout(activity_contact_form.this);
+                    @Override
+                    public void onComplete(@NonNull @NotNull Task<UploadTask.TaskSnapshot> task) {
+                        fileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                            @Override
+                            public void onSuccess(Uri uri) {
+                                String url = uri.toString();
+
+                                Log.d("DownloadUrl", url);
+                                pd.dismiss();
+                                Toast.makeText(activity_contact_form.this, "Image Upload Successful", Toast.LENGTH_SHORT).show();
+//                                TextView dynamic_url_text = new TextView(activity_contact_form.this);
+//                                dynamic_url_text.setLayoutParams(new AbsListView.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ActionBar.LayoutParams.WRAP_CONTENT));
+//                                dynamic_url_text.setText("URL");
+//                                setContentView(l1);
+                                TextView textView = (TextView)findViewById(R.id.dynamic);
+                                String display = "Image Uploaded";
+                                textView.setText(display);
+                            }
+                        });
+                    }
+                });
+            }
+
+        }
     }
 }
